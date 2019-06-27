e52075a7365276832a65bef505dd2338f277b1a2
zeue <zeue@anon.zeue.net>
2019-06-27 10:20:22 +0000

    Update 'src/Controller/PublicController.php'

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -44,7 +44,7 @@ class PublicController extends AbstractController
         ]);
         $storage->registerStreamWrapper();
 
-        $galleryImages = array_diff(scandir("gs://files.uag.zeue.net/images/gallery"), array('images/gallery/'));
+        $galleryImages = array_diff(scandir("gs://files.unnamed.group/images/gallery"), array('images/gallery/'));
 
         shuffle($galleryImages);
 

b701a6e85357924a2004a0e3dfe7e58d63e9f504
zeue <zeuedude@gmail.com>
2020-02-17 20:20:05 +0000

    handbook: darkmode is now default on && fixed build errors

--- handbook/docs/.vuepress/darkreader.js
+++ handbook/docs/.vuepress/darkreader.js
@@ -2,46 +2,52 @@ module.exports = () => {
   return {
     enhanceAppFiles() {
       return {
-         name: 'darkreader',
-         content: `export default () => {
-           window.addEventListener('load', function () {
-             var DarkReader = require("darkreader");
-             var Cookies = require("js-cookie");
-
-             var darkModeOptions = {
-                 brightness: 150,
-                 contrast: 80,
-                 sepia: 0,
-             };
-
-             var toggler = document.createElement("a");
-
-             toggler.classList.add("repo-link");
-             toggler.href = "javascript:null";
-
-             if (Cookies.get("darkModeToggle") === "1") {
-                 DarkReader.enable(darkModeOptions);
-                 toggler.innerHTML = "Light Mode";
-             } else {
-                 toggler.innerHTML = "Dark Mode";
-             }
-
-             toggler.addEventListener("click", function (e) {
-                 if (Cookies.get("darkModeToggle") === "1") {
-                     DarkReader.disable()
-                     Cookies.set("darkModeToggle", "0", { expires: 30 });
-                     this.innerHTML = "Dark Mode";
-                 } else {
-                     DarkReader.enable(darkModeOptions);
-                     Cookies.set("darkModeToggle", "1", { expires: 30 });
-                     this.innerHTML = "Light Mode";
-                 }
-             });
-
-             document.body.getElementsByClassName('nav-links')[0].appendChild(toggler);
-           })
+        name: "darkreader",
+        content: `export default () => {
+           try {
+            window.addEventListener('load', function () {
+              var DarkReader = require("darkreader");
+              var Cookies = require("js-cookie");
+ 
+              var darkModeOptions = {
+                  brightness: 100,
+                  contrast: 100,
+                  sepia: 0,
+              };
+ 
+              var toggler = document.createElement("a");
+ 
+              toggler.classList.add("repo-link");
+              toggler.href = "javascript:null";
+ 
+              if (Cookies.get("darkModeToggle") === undefined) {
+               Cookies.set("darkModeToggle", "1", { expires: 30 });
+              }
+ 
+              if (Cookies.get("darkModeToggle") === "1") {
+                  DarkReader.enable(darkModeOptions);
+                  toggler.innerHTML = "Light Mode";
+              } else {
+                  toggler.innerHTML = "Dark Mode";
+              }
+ 
+              toggler.addEventListener("click", function (e) {
+                  if (Cookies.get("darkModeToggle") === "1") {
+                      DarkReader.disable()
+                      Cookies.set("darkModeToggle", "0", { expires: 30 });
+                      this.innerHTML = "Dark Mode";
+                  } else {
+                      DarkReader.enable(darkModeOptions);
+                      Cookies.set("darkModeToggle", "1", { expires: 30 });
+                      this.innerHTML = "Light Mode";
+                  }
+              });
+ 
+              document.body.getElementsByClassName('nav-links')[0].appendChild(toggler);
+            })
+           } catch (e) {}
          }`
-       }
+      };
     }
-  }
-}
+  };
+};
